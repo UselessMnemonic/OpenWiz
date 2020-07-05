cb28b072897546b203a50d79e98c8b686fc8ef13
Christopher Madrigal <nihil@Nihil.local>
2020-07-04 22:13:24 -0700

    Test now implements discovery interface

--- test/TestWizDiscovery.cs
+++ test/TestWizDiscovery.cs
@@ -1,12 +1,34 @@
 using OpenWiz;
 using System;
+using System.Threading;
 using System.Text;
+using System.Collections.Concurrent;
 
-public class TestWizDiscovery {
+public class TestWizDiscovery : IWizDiscoveryListener {
+
+    private ConcurrentDictionary<string, WizHandle> handles;
+
+    public TestWizDiscovery()
+    {
+        handles = new ConcurrentDictionary<string, WizHandle>();
+    }
+
+    public void OnDiscover(WizHandle discovered)
+    {
+        if (!handles.ContainsKey(discovered.Mac))
+        {
+            Console.WriteLine($"[INFO] TestWizDiscovery::OnDiscover found light {discovered.Mac}");
+            handles.TryAdd(discovered.Mac, discovered);
+        }
+    }
+
     public static void Main(string[] args)
     {
-        WizDiscoveryService wds = new WizDiscoveryService(390198, "192.128.0.100", "F01898091AD8");
-        wds.Start();
+        TestWizDiscovery twd = new TestWizDiscovery();
+        WizDiscoveryService wds = new WizDiscoveryService(390198, "192.168.0.100", new byte[]{ 0xF0, 0x18, 0x98, 0x09, 0x1A, 0xD8});
+
+        wds.Start(twd);
+        
         while (true) {}
     }
 }
