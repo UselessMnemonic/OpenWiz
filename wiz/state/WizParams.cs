8d75b1047df2ba0bb895441fa05e2e1c512b572e
Christopher Madrigal <nihil@Nihil.local>
2020-07-05 21:21:26 -0700

    Merge Param and Result objects

--- wiz/state/WizParams.cs
+++ wiz/state/WizParams.cs
@@ -31,5 +31,76 @@ namespace OpenWiz
         /// <value>true if a register request is successful; false or null, otherwise.</value>
         /// 
         public bool? Register { get; set; }
+
+        /// <summary>
+        /// The MAC of the remote light.
+        /// </summary>
+        /// <value>A string, or null if none is given</value>
+        /// 
+        public string Mac { get; set; }
+
+        /// <summary>
+        /// Whether the remote light is on or off.
+        /// </summary>
+        /// <value>true if the light is on, false otherwise,
+        /// or null if no value is given.</value>
+        /// 
+        public bool? State { get; set; }
+
+        /// <summary>
+        /// The Scene ID of the current scene.
+        /// </summary>
+        /// <value>An int, or null if no scene is active.</value>
+        /// 
+        public int? SceneId { get; set; }
+
+        /// <summary>
+        /// The current speed at which a scene plays.
+        /// </summary>
+        /// <value>An int, or null if no value is given.</value>
+        /// 
+        public int? Speed { get; set; }
+
+        /// <summary>
+        /// The current Red component of the set color.
+        /// </summary>
+        /// <value>An int, or null if no value is given.</value>
+        /// 
+        public int? R { get; set; }
+
+        /// <summary>
+        /// The current Green component of the set color.
+        /// </summary>
+        /// <value>An int, or null if no value is given.</value>
+        /// 
+        public int? G { get; set; }
+
+        /// <summary>
+        /// The current Blue component of the set color.
+        /// </summary>
+        /// <value>An int, or null if no value is given.</value>
+        /// 
+        public int? B { get; set; }
+
+        /// <summary>
+        /// The current Cool White component of the set color.
+        /// </summary>
+        /// <value>An int, or null if no value is given.</value>
+        /// 
+        public int? C { get; set; }
+
+        /// <summary>
+        /// The current Warm White component of the set color.
+        /// </summary>
+        /// <value>An int, or null if no value is given.</value>
+        /// 
+        public int? W { get; set; }
+
+        /// <summary>
+        /// The dimming/intensity of the light.
+        /// </summary>
+        /// <value>An int, or null if no value is given.</value>
+        /// 
+        public int? Dimming { get; set; }
     }
 }
--- wiz/state/WizResult.cs
+++ wiz/state/WizResult.cs
@@ -2,15 +2,8 @@ namespace OpenWiz
 {
     /// <summary>Class <c>WizResult</c> stores result information in a <c>WizState</c></summary>
     ///
-    public class WizResult
+    public class WizResult : WizParams
     {
-        /// <summary>
-        /// The Mac of the remote light, as a, unformatted lowercase hex string.
-        /// </summary>
-        /// <value>A hex string, or null if none is given.</value>
-        /// 
-        public string Mac { get; set; }
-
         /// <summary>
         /// Wether a method call was successful.
         /// </summary>
